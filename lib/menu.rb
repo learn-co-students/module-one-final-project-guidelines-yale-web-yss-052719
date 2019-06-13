27eb7ee4301a3e711825aa02249164624f5e085e
leoniedong <leoniedong@hotmail.com>
2019-06-12 23:26:50 -0400

    0612pm

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,7 +5,7 @@ end
 def create_list(name)
     list = List.new
     list.name = name
-    list.user_id = $user.id
+    $user.lists << list
     list.save
 end
 
@@ -14,7 +14,7 @@ def start
     prompt = TTY::Prompt.new
     option = prompt.select("Choose your option:") do |menu|
         menu.choice 'Login', -> do
-            login_user
+            login
         end
         
         menu.choice 'Search for a book', -> do
@@ -49,16 +49,37 @@ end
 def login_user
     prompt = TTY::Prompt.new
     input = prompt.ask('What is your username?', required: true)
-    clear_screen
     if User.all.find_by(username: input)
         $user = User.all.find_by(username: input)
         clear_screen
         profile
     else
-        option = prompt.select("You don't seem to have an account. Would you like to create one?") do |menu|
+        input = prompt.select("You don't seem to have an account. Would you like to create one?") do |menu|
             menu.choice 'Yes', -> do
                 $user = User.create(username: input)
                 profile
+            end
+            menu.choice 'No', -> do
+                quit
+            end
+        end
+    end
+end
+
+def login
+    prompt = TTY::Prompt.new
+    input = prompt.ask('What is your username?', required: true)
+    if User.all.find_by(username: input)
+        $user = User.all.find_by(username: input)
+        clear_screen
+        profile
+        next_step
+    else
+        input = prompt.select("You don't seem to have an account. Would you like to create one?") do |menu|
+            menu.choice 'Yes', -> do
+                $user = User.create(username: input)
+                profile
+                next_step
             end
             menu.choice 'No', -> do
                 quit
@@ -79,7 +100,7 @@ end
 
 def list(books)
     books.each_with_index do |book, i|
-        puts "#{i+1}. #{book.book_title}"
+        puts "#{book.id}. #{book.book_title}"
     end
 end
 
@@ -89,12 +110,11 @@ def search_book
         menu.enum '.'
         menu.choice 'Author name', -> do
             input = prompt.ask('Enter author name: ', required: true)
-            if Book.all.map {|book| book.author_name}.include?("#{input}")
+            books_found = Book.where(author_name: input)
+            if !books_found.empty?
                 clear_screen
-                books = []
-                books << Book.all.find_by(author_name: "#{input}")
                 puts "We have found the following book(s):"
-                list(books)
+                list(books_found)
                 store_book
             else
                 puts "Sorry, we found no matching results"
@@ -104,18 +124,16 @@ def search_book
         end
 
         menu.choice 'Author genre', -> do
+
         end
 
-        menu.choice 'Book Rating', -> do
-        end
 
         menu.choice 'Book Title', -> do
+
         end
 
         menu.choice 'Genre', -> do
-        end
 
-        menu.choice 'Publication Date', -> do
         end
 
         menu.choice 'Exit', -> do
@@ -138,35 +156,46 @@ end
 #     end
 # end
 
-def select_book
+# def select_book
+#     prompt = TTY::Prompt.new
+#     input = prompt.ask('Please select a book by entering the book name.', required: true)
+#     $book = $books_found.find{|book| book.book_title = "#{input}"}
+#     "Book information: #{$book}"
+# end
+
+def select_book_by_index
     prompt = TTY::Prompt.new
-    input = prompt.ask('Please select a book by entering the book name.', required: true)
-    $book = Book.all.find_by(book_title: "#{input}")
-    puts "Book information:"
-    $book
+    input = prompt.ask('Please select a book by entering its id.', required: true)
+    book = Book.find(input)
+    puts "Book information: #{book.book_title}, #{book.author_name}"
+    return book
 end
 
 def store_book
     prompt = TTY::Prompt.new
-    select_book
-    input = prompt.select("Would you like to store the book to your your list?", required: true) do |menu|
+    book = select_book_by_index
+    input = prompt.select("Would you like to store the book to your list?", required: true) do |menu|
         menu.choice 'Yes', -> do
             login_user
             if $user.lists == []
                 input = prompt.ask("You don't have any lists. Please create a new list. Enter the name of your new list: ") 
                 create_list("#{input}")
-                add_book_to_list($book, "#{input}")
+                add_book_to_list(book, "#{input}")
+                puts "Your book #{book.book_title} is successfully stored in your list #{input}."
+                next_step
             else input = prompt.ask("Please enter a list name. ")
                 if $user.lists_names.include?("#{input}")
-                    add_book_to_list($book, "#{input}")
-                    puts "Your book #{$book} is successfully stored in your list #{input}."
+                    add_book_to_list(book, "#{input}")
+                    puts "Your book #{book.book_title} is successfully stored in your list #{input}."
+                    next_step
                 else
                     puts input = prompt.select("The list you entered doesn't seem to exist. Would you like to create one?") do |menu|
                         menu.choice 'Yes', -> do
                             input = prompt.ask("Enter the name for your list: ", required: true)
                             $user.create_list(input)
-                            add_book_to_list($book, "#{input}")
+                            add_book_to_list(book, "#{input}")
                             puts "Your book is successfully stored in your list #{input}."
+                            next_step
                         end
                         menu.choice 'No', -> do 
                             next_step
@@ -204,6 +233,21 @@ def next_step
             next_step
         end
 
+        menu.choice 'Show my lists', -> do
+            puts "#{$user.lists.map {|list| list.name}}"
+            next_step
+        end
+
+        menu.choice 'Show my books', -> do
+            hash = Hash.new
+            $user.lists.each do |list|
+                hash[list.name] = list.id
+            end
+            input = prompt.select("Select a list:", hash)
+            puts "#{List.find(input).books.map {|book| book.name}}"
+            next_step
+        end
+
         menu.choice 'Search for a book', -> do
             search_book
         end
@@ -214,9 +258,9 @@ def next_step
     end
 end
 
-def add_book_to_list(bookname, listname)
+def add_book_to_list(book, listname)
     list_book = ListBook.new
-    list_book.book = $book
+    list_book.book = book
     list_book.list = List.find_by(name: "#{listname}")
     list_book.save
 end
